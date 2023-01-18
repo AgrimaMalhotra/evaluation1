e02b8623e2ad95f4465d0116ec68510cb7c5f303
Agrima <er.agrima.malhotra@gmail.com>
2023-01-18 14:06:47 +0530

    feat: add track bowling game score

--- bowlingScore.js
+++ bowlingScore.js
@@ -1,57 +1,57 @@
 function calculateScore(roundScore) {
-    let index = 0;
-    let eachRoundScore, totalScore = 0;
+  let index = 0;
+  let eachRoundScore, totalScore = 0;
 
-    while (index < 10) {
-        eachRoundScore = roundScore[index].reduce((a, b) => a + b, 0);
-        if (index == 9) {
-            //10th round
-            totalScore += eachRoundScore;
-            break;
-        }
-        if (eachRoundScore < 10) {//open round
-            totalScore += eachRoundScore;
-        }
-        else if (roundScore[index][1] === 10) {
-            //spare
-            totalScore += 10 + roundScore[index + 1][0];
-        }
-        else if (roundScore[index][0] == 10) {
-            //strike
-            totalScore += 10 + roundScore[index][1] + roundScore[index + 1][0];
-        }
-        index += 1;
-
+  while (index < 10) {
+    eachRoundScore = roundScore[index].reduce((a, b) => a + b, 0);
+    if (index == 9) {
+      //10th round
+      totalScore += eachRoundScore;
+      break;
     }
-    return totalScore;
+    if (eachRoundScore < 10) {//open round
+      totalScore += eachRoundScore;
+    }
+    else if (roundScore[index][1] === 10) {
+      //spare
+      totalScore += 10 + roundScore[index + 1][0];
+    }
+    else if (roundScore[index][0] == 10) {
+      //strike
+      totalScore += 10 + roundScore[index][1] + roundScore[index + 1][0];
+    }
+    index += 1;
+
+  }
+  return totalScore;
 
 }
 
 function score(rolls) {
-    if (!Array.isArray(rolls) || rolls.length == 0) {
-        throw new Error('Invalid array');
+  if (!Array.isArray(rolls) || rolls.length == 0) {
+    throw new Error('Invalid array');
+  }
+  let roundScore = {};
+  for (let index = 0; index < rolls.length; index += 2) {
+    if (index == 18) {
+      roundScore[parseInt(index / 2)] = rolls.slice(index,);
+      break;
     }
-    let roundScore = {};
-    for (let index = 0; index < rolls.length; index += 2) {
-        if (index == 18) {
-            roundScore[parseInt(index / 2)] = rolls.slice(index,);
-            break;
-        }
-        roundScore[parseInt(index / 2)] = rolls.slice(index, index + 2);
-    }
-    if (Object.keys(roundScore).length != 10) {
-        throw new Error('Invalid');
-    }
+    roundScore[parseInt(index / 2)] = rolls.slice(index, index + 2);
+  }
+  if (Object.keys(roundScore).length != 10) {
+    throw new Error('Invalid');
+  }
 
-    return calculateScore(roundScore);
+  return calculateScore(roundScore);
 
 }
 function bestScore(games) {
-    let max = score(games[0]);
-    for (let index = 1; index < games.length; index++) {
-        max = Math.max(max, score(games[index]));
-    }
-    return max;
+  let max = score(games[0]);
+  for (let index = 1; index < games.length; index++) {
+    max = Math.max(max, score(games[index]));
+  }
+  return max;
 }
 
 var rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
